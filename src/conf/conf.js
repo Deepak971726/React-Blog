33877c5d02619fa485612689ce4e89cccab7d8c5
Deepak <dy705327@gmail.com>
2025-12-29 20:40:37 +0530

    message

---
+++ src/conf/conf.js
@@ -0,0 +1,11 @@
+const conf={
+    appwriteUrl:String(import.meta.env.appwriteUrl),
+    appwriteProjectId:String(import.meta.env.appwriteProjectId),
+    appwriteDatabaseId:String(import.meta.env.appwriteDatabaseId),
+    appwriteCollectionId:String(import.meta.env.appwriteCollectionId),
+    appwriteBucketId:String(import.meta.env.appwriteBucketId)
+}
+
+
+
+export default conf;
