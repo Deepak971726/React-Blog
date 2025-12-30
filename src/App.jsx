86626e055d119a563a236c5e8f6147263dbeff14
Deepak <dy705327@gmail.com>
2025-12-30 13:23:24 +0530

    components-commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,56 @@
  
+import { useEffect, useState } from 'react'
 import './App.css'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import {Header, Footer} from './components/index'
+import { Outlet } from 'react-router-dom'
 
 function App() {
+  
+  const [loading, setLoading] = useState(false)
+  const dispatch = useDispatch()
+  
+  useEffect(()=>{
+    authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+        // setLoading(false)
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=> setLoading(false))
+    
+    // console.log(loading)
+  },[])
+  
 
-  return (
-    <>
-       <div>
-        <h1>Mega Blog with appwrite</h1>
-       </div>
-    </>
+  return !loading?(
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+        <div className='w-full block'>
+          <Header/>
+          <main>
+            TODO:{/* <Outlet/> */}
+          </main>
+          <Footer/>
+          
+        </div>      
+    </div>
+  ):(
+    <div><h1>Page not found</h1></div>
   )
+  
+  // return (
+  //   <div>
+  //     <h1>
+  //       this is my new project
+  //     </h1>
+  //   </div>
+  // )
 }
 
 export default App
--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,9 +1,9 @@
 const conf={
-    appwriteUrl:String(import.meta.env.appwriteUrl),
-    appwriteProjectId:String(import.meta.env.appwriteProjectId),
-    appwriteDatabaseId:String(import.meta.env.appwriteDatabaseId),
-    appwriteCollectionId:String(import.meta.env.appwriteCollectionId),
-    appwriteBucketId:String(import.meta.env.appwriteBucketId)
+    appwriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDatabaseId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID)
 }
 
 
